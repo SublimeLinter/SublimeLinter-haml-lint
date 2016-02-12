be0fd599ad1e8c8a155478baf6438043201ab434
Kamen Hursev <kamen@currencytransfer.com>
2016-02-12 17:20:39 +0200

    Fix an issue with locating Rubocop config

--- linter.py
+++ linter.py
@@ -10,11 +10,11 @@
 
 """This module exports the HamlLint plugin class."""
 
-from SublimeLinter.lint import RubyLinter
+import os
+from SublimeLinter.lint import RubyLinter, util
 
 
 class HamlLint(RubyLinter):
-
     """Provides an interface to haml-lint."""
 
     syntax = 'ruby haml'
@@ -25,3 +25,25 @@ class HamlLint(RubyLinter):
     regex = r'^.+?:(?P<line>\d+) \[(:?(?P<warning>W)|(?P<error>E))\] (?P<message>.+)'
     tempfile_suffix = 'haml'
     config_file = ('--config', '.haml-lint.yml')
+
+    def build_args(self, settings):
+        """
+        Return a list of args to add to cls.cmd.
+
+        We hook into this method to find the rubocop config and set it as an
+        environment variable for the rubocop linter to pick up.
+        """
+        rubocop_file = settings.get('rubocop-config', '.rubocop.yml')
+        if self.filename:
+            rubocop_config = util.find_file(
+                os.path.dirname(self.filename),
+                rubocop_file,
+                aux_dirs='~'
+            )
+            print(rubocop_config)
+            if rubocop_config:
+                self.env['HAML_LINT_RUBOCOP_CONF'] = rubocop_config
+            else:
+                self.env.pop('HAML_LINT_RUBOCOP_CONF', None)
+
+        return super().build_args(settings)
